6b10deda562a364a46af2d4dd4d3e9eb5735cbef
JohnnySimple <obengboatengjohnson@gmail.com>
2023-12-02 23:47:09 +0000

    add: add updates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
     return HttpResponse("You know how we do it. Let's go to the mars🚀")
+
+
+def detail(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    return HttpResponse("You are looking at question %s." % question)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
