989678594785282bf51f6781f2e7e778e110eaa7
JohnnySimple <obengboatengjohnson@gmail.com>
2023-12-03 09:28:39 +0000

    add: add update

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,14 +1,24 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Question
 
 def index(request):
-    return HttpResponse("You know how we do it. Let's go to the mars🚀")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {
+        "latest_question_list": latest_question_list
+    }
+
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
-    question = Question.objects.get(pk=question_id)
-    return HttpResponse("You are looking at question %s." % question)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
